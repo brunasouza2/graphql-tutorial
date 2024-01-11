c6ec8675cc827bc7f31f6f366782ee9e5c6e05b6
Bruna Souza <bruna.ssouza2@gmail.com>
2024-01-10 22:00:25 -0300

    initial configs

---
+++ src/main/java/com/graphqljava/tutorial/bookDetails/BookController.java
@@ -0,0 +1,27 @@
+package com.graphqljava.tutorial.bookDetails;
+
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.graphql.data.method.annotation.SchemaMapping;
+import org.springframework.stereotype.Controller;
+
+import java.util.List;
+
+@Controller
+public class BookController {
+    @QueryMapping
+    public Book bookById(@Argument String id) {
+        return Book.getById(id);
+    }
+
+    @QueryMapping
+    public List<Book> books() {
+        return Book.list();
+    }
+
+
+    @SchemaMapping
+    public Author author(Book book) {
+        return Author.getById(book.getAuthorId());
+    }
+}
